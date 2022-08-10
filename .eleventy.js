0e954de9a6a8c58bebd031cbb7d744f9fddd6131
nwa-second-stage <nwasecondstage@gmail.com>
2022-08-09 20:33:56 -0500

    restructured folders/files

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,9 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addWatchTarget("./src/css/");
-    eleventyConfig.addPassthroughCopy({ "src/_assets": "assets" });
+    eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+    eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+    eleventyConfig.addPassthroughCopy({ "src/javascript": "javascript" });
     eleventyConfig.addPassthroughCopy("./src/robot.txt");
     eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
     eleventyConfig.addPassthroughCopy("./src/sitemap.xsl");
@@ -32,57 +33,3 @@ module.exports = function (eleventyConfig) {
       },
     };
   };
-// new addition
-const Purgecss = require('purgecss')
-const { JSDOM } = require('jsdom')
-const CleanCSS = require("clean-css");
-
-//array of css files to combine
-const cssFiles = ['./src/_includes/custom.css','./src/css/bootstrap51.css']
-
-// cleanCSSOptions for minification and inlining css, will fix duplicate media queries
-const cleanCSSOptions = {
-  level: {
-    2: {
-      all: true, 
-      removeDuplicateRules: true 
-    }
-  }
-}
-
-//function to insert css into the DOM
-const insertCss = (html, css) => {
-  const dom = new JSDOM(html)
-  const { document } = dom.window
-
-  let head = document.getElementsByTagName('head')[0];
-  let style = document.createElement("style");
-  style.type = 'text/css';
-  style.innerHTML = css;
-  head.appendChild(style);
-
-  return dom.serialize()
-}
-
-module.exports = function (eleventyConfig) {  
-  eleventyConfig.addTransform("purgeCSS", function(content, outputPath){
-    if( outputPath.endsWith(".html") ) {
-      console.log(outputPath)
-      const purgecss = new Purgecss({
-        content: [outputPath],
-        css: cssFiles
-      })
-      const purgecssResult = purgecss.purge()
-      let cssMerge = ''
-      if(purgecssResult.length>0){
-        for (let i = 0; i < purgecssResult.length; i++){
-          cssMerge= cssMerge.concat(purgecssResult[i].css)
-        }
-        const cssMin = new CleanCSS(cleanCSSOptions).minify(cssMerge).styles
-        return insertCss(content, cssMin)
-      }
-    }
-    return content
-
-  })
-}
